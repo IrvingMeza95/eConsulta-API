3680df366aa0c18f19dc15eed6c211ec95bb42ce
Irving Meza <irvingmeza95@gmail.com>
2025-01-19 16:33:16 -0600

    Se cambio el archivo init.

--- init.sql
+++ init.sql
@@ -1,4 +1,6 @@
 CREATE DATABASE IF NOT EXISTS econsulta_db;
+CREATE DATABASE IF NOT EXISTS econsulta_db_archivos_db;
 INSERT INTO econsulta_db.roles (id, nombre) 
 VALUES (1, 'ROLE_ADMIN');
-CREATE DATABASE IF NOT EXISTS econsulta_db_archivos_db;
+INSERT INTO econsulta_db.roles (id, nombre) 
+VALUES (2, 'ROLE_USER');
