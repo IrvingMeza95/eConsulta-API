50c08631a9b5510cce1dd0d6c510221b3c27cefe
Irving Meza <irvingmeza95@gmail.com>
2025-01-26 13:26:35 -0600

    Se agregaron configs para servicio verificacion.

--- init.sql
+++ init.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS econsulta_db;
-CREATE DATABASE IF NOT EXISTS econsulta_db_archivos_db;
+CREATE DATABASE IF NOT EXISTS econsulta_archivos_db;
 INSERT INTO econsulta_db.roles (id, nombre) 
 VALUES (1, 'ROLE_ADMIN');
 INSERT INTO econsulta_db.roles (id, nombre) 
