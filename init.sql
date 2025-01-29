fb2ba16e2e4aa16839c757eded5f1836bbd2f39f
Irving Meza <irvingmeza95@gmail.com>
2025-01-28 20:30:21 -0600

    Se modifico el docker-compose para permitir los reinicios automaticos.

--- init.sql
+++ init.sql
@@ -1,6 +1,2 @@
 CREATE DATABASE IF NOT EXISTS econsulta_db;
 CREATE DATABASE IF NOT EXISTS econsulta_archivos_db;
-INSERT INTO econsulta_db.roles (id, nombre) 
-VALUES (1, 'ROLE_ADMIN');
-INSERT INTO econsulta_db.roles (id, nombre) 
-VALUES (2, 'ROLE_USER');
